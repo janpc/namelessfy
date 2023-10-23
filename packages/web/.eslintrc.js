c9b4ba0d614c5e799b03545563b735c24370da6e
janpc <janpuigcapella@gmail.com>
2023-10-23 20:18:12 +0200

    copy project

--- packages/web/.eslintrc.js
+++ packages/web/.eslintrc.js
@@ -52,6 +52,7 @@ module.exports = {
     "no-underscore-dangle": "off",
     "react/forbid-prop-types": "off",
     "react/prop-types": "off",
+    "react/jsx-no-bind": "off",
     "jsx-a11y/label-has-associated-control": [
       "error",
       {
